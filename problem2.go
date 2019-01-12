e4fee6bf6a17b201fb238b8e5af98d7229e2c786
Mario Eidher <me@marios-air.home>
2019-01-12 13:13:43 +0100

    solutions to problem1 and problem2

---
+++ problem2.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	max := 4000000
+	sum := 0
+	term1 := 1
+	term2 := 2
+
+	for {
+		term1, term2 = nextFib(term1, term2)
+		if term1%2 == 0 {
+			sum += term1
+		}
+		if term2 > max {
+			break
+		}
+	}
+	fmt.Println(sum)
+}
